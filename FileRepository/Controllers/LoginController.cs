47def5e86aacd0579375cd0014f657ad7d5d9b08
Krzysztof Tybinka <ktybinka@interia.pl>
2023-11-12 00:06:03 +0100

    Controllers

---
+++ FileRepository/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileRepository.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FileRepository/Controllers/RegisterController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileRepository.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
